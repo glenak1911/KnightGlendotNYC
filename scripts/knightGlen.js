d3a7b241a4d4f705fa00127b542401b09259071e
Glen Knight <glenak1911@gmail.com>
2016-05-11 18:53:07 -0400

    Add'l styling changes

--- scripts/knightGlen.js
+++ scripts/knightGlen.js
@@ -1,3 +1,5 @@
 function displayContactInfo(){
 		document.getElementById("contactInfo").style.visibility = "visible";
+		document.getElementById("contactInfo").style.WebkitTransition =  'all .5s ease';
+		document.getElementById("contactInfo").style.transition = 'all .5s ease';
 }
