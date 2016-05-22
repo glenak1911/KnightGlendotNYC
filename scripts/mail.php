880ffdece3fd515ca1ec91c27c65f8bd89092b87
Glen Knight <glenak1911@gmail.com>
2016-05-22 13:40:50 -0400

    Adding mail script and htaccess

---
+++ scripts/mail.php
@@ -0,0 +1,11 @@
+<?php 
+$ToEmail = 'glenak1911@gmail.com'; 
+$EmailSubject = 'KnightGlen.com Inquiry'; 
+$mailheader = "From: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+$MESSAGE_BODY = "Name: ".$_POST["name"].""; 
+$MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
+$MESSAGE_BODY .= "Comment: ".nl2br($_POST["comment"]).""; 
+mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
+?>
