c0204af38031298f3ecedf1bc8d2ce1cd793d202
Glen Knight <glenak1911@gmail.com>
2016-05-23 00:20:03 -0400

    Modified email script to add confirmation message and remediate unknown sender issue

--- scripts/mail.php
+++ scripts/mail.php
@@ -1,11 +1,15 @@
 <?php 
 $ToEmail = 'glenak1911@gmail.com'; 
 $EmailSubject = 'KnightGlen.com Inquiry'; 
-$mailheader = "From: ".$_POST["formEmail"]."\r\n"; 
+$mailheader = "From: ".$_POST["name"]."\r\n"; 
 $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
 $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-$MESSAGE_BODY = "Name: ".$_POST["name"]."\n"; 
-$MESSAGE_BODY .= "Email: ".$_POST["email"]."\n"; 
+$MESSAGE_BODY = "Name: ".$_POST["name"]."\r\n"; 
+$MESSAGE_BODY .= "Email: ".$_POST["email"]."\r\n"; 
 $MESSAGE_BODY .= "Comment: ".nl2br($_POST["comment"]).""; 
-mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
+mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("There was an error, please try again"); 
+
+echo '<p>Your message has been sent!<br>I will get in touch with you shortly!</p>';
+echo '<a href="http://knightglen.com">Click here to return</p>';
 ?>
+
