0d015f48aba22b5364a1f1af074f9d0731f1c5cd
Glen Knight <glenak1911@gmail.com>
2018-03-22 07:10:08 -0400

    cleanup

--- front-page.php
+++ front-page.php
@@ -24,14 +24,14 @@
 			<div class="portfolioContent">
 				<hr />
 				<h1 id="Portfolio">Portfolio</h1>
-				<div class="col-md-4">
+				<div class="col-md-4 portfolioblock">
 					<img src="<?php echo get_bloginfo('template_directory'); ?>/img/gamma.png" alt="Portfolio Item" class="portfolioItem">
 					<h2>Gamma:Photo Sharing App</h2>
 					<p>A demo site for a ficticious photo sharing app.</p>
 					<a class="btn btn-default btn-lg" href="<?php echo get_bloginfo('template_directory'); ?>/photoAppSite/index.html" role="button">View Site</a>
 					<a class="btn btn-default btn-lg" href="https://github.com/glenak1911/photoAppSite" role="button">View Code</a>
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-4 portfolioblock">
 					<img src="<?php echo get_bloginfo('template_directory'); ?>/img/yannipaul.png" alt="Portfolio Item" class="portfolioItem">
 					<h2>Yanni Paul</h2>
 					<p>A Place For Those With Too Many Interests</p>
