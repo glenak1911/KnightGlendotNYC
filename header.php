c629f31f2878d961c5bd6cd8a57bad06c69594c7
Glen Knight <glenak1911@gmail.com>
2018-03-22 06:23:46 -0400

    cleanup

--- header.php
+++ header.php
@@ -34,12 +34,14 @@
 						<li class="navigationItem"><a class ="navLink" href="<?php echo get_bloginfo( 'wpurl' );?>/blog">Blog</a></li>
 						<li class="navigationItem"><a class ="navLink" href="<?php echo get_bloginfo( 'wpurl' );?>/docs/Resume.pdf">Resume</a></li>
 					</ul>
-					<div class="input-group">
-              <span class="input-group-btn">
-                  <button class="btn btn-default" type="button">Go!</button>
-              </span>
-              <input type="text" class="form-control" placeholder="Search for...">
-          </div><!-- /input-group -->
+					 <div class="col-md-3">
+					    <div class="input-group">
+                  <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">Go!</button>
+                  </span>
+                  <input type="text" class="form-control" placeholder="Search for...">
+              </div><!-- /input-group -->
+					</div>
 				</div>
 			</div>
 		</nav>
