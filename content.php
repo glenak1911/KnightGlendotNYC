f7af6246574f75bb68810e53617b075cc7e35f98
Glen A Knight <glenak1911@gmail.com>
2018-04-11 10:06:06 -0400

    remove link to author and add category

--- content.php
+++ content.php
@@ -1,6 +1,7 @@
 <div class="blog-post">
   <h2 class="blog-post-title"><?php the_title(); ?></h2>
-	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+	<p class="blog-post-mets"><?php get_the_category(); ?></p>
+	<p class="blog-post-meta"><?php the_date(); ?> by <?php the_author(); ?></p>
 
  <?php the_excerpt(); ?>
 
